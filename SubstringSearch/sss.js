720b35f660de1e5009729daacf6a7793e76d69a0
Maxim <mbrozovskii@gmail.com>
2023-04-26 21:15:25 +0500

    Substring: minor fixes

--- SubstringSearch/sss.js
+++ SubstringSearch/sss.js
@@ -1,16 +1,9 @@
-const string = "abcdcab";
-const pattern = "ab";
-const string2 = "Addabffg";
-const pattern2 = "ffg";
-const string1 = "the quick brown fox jumps over the lazy dog";
-const pattern1 = "fox";
-
 function bruteforce(string, pattern) {
   let ans = [];
-  seraching: for (let i = 0; i <= string.length - pattern.length; ++i) {
+  searching: for (let i = 0; i <= string.length - pattern.length; ++i) {
     for (let j = 0; j < pattern.length; ++j) {
       if (string[i + j] !== pattern[j]) {
-        continue seraching;
+        continue searching;
       }
     }
     ans.push(i);
@@ -44,6 +37,13 @@ function rabin_karp(string, pattern, p = 5) {
   }
   return ans;
 }
+
+const string = "abcdcab";
+const pattern = "ab";
+const string2 = "Addabffg";
+const pattern2 = "ffg";
+const string1 = "the quick brown fox jumps over the lazy dog";
+const pattern1 = "fox";
 
 console.log(rabin_karp(string, pattern));
 console.log(rabin_karp(string1, pattern1));
